e8d6ed51f09a380d16aa9df29e311f6cf596ae24
shadow-developper <axel.barth@yahoo.com>
2025-08-23 13:16:23 +0200

    CSS Styling - Chapter 02

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -3,27 +3,14 @@ import clsx from 'clsx';
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
-    <span
+        <span
       className={clsx(
-        'inline-flex items-center rounded-full px-2 py-1 text-xs',
+        'inline-flex items-center rounded-full px-2 py-1 text-sm',
         {
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
         },
       )}
-    >
-      {status === 'pending' ? (
-        <>
-          Pending
-          <ClockIcon className="ml-1 w-4 text-gray-500" />
-        </>
-      ) : null}
-      {status === 'paid' ? (
-        <>
-          Paid
-          <CheckIcon className="ml-1 w-4 text-white" />
-        </>
-      ) : null}
-    </span>
+    ></span>
   );
 }
