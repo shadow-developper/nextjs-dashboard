cfec820a8fe7529780de1e984dd4bbb9812387ce
shadow-developper <axel.barth@yahoo.com>
2025-08-26 10:59:29 +0200

    Partial prerendering - Chapter 10

--- app/page.tsx
+++ app/page.tsx
@@ -25,9 +25,13 @@ export default function Page() {
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
+          <Link
+          href="/dashboard"
+          className="flex items-center gap-5 self-start rounded-lg bg-green-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-green-400 md:text-base">
+            <span>Accéder au dashboard</span>
+          </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
                 <Image
                   src="/hero-desktop.png"
                   width={1000}
